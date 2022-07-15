04182f16c221a7e03f86cdc147c60de35da23436
MJWORKS <mjworks2020@outlook.com>
2022-07-15 11:17:09 -0300

    Added new changes

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,10 @@
+import { CalculatorService } from './calculator.service';
+describe(CalculatorService.name, () => {
+  it('should add two numbers', () => {
+    pending();
+  });
+
+  it('should substract two numbers', () => {
+    pending();
+  });
+});
