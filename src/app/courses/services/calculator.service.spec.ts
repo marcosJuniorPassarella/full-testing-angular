427ffce1cc6ed4bc9d7d4f191149337f8805902e
MJWORKS <mjworks2020@outlook.com>
2022-07-16 14:13:32 -0300

    Added beforeEach example

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,18 +1,27 @@
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 describe(CalculatorService.name, () => {
+  let calculator: CalculatorService;
+  let loggerSpy: any;
+
+  beforeEach(() => {
+    console.log('calling before each');
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  });
+
   it('should add two numbers', () => {
-    const logger = jasmine.createSpyObj('LoggerService', ['log']);
-    // vc pode utilizar para enviar como retorno um mock de dados
-    //logger.log.and.returnValue();
-    const calcularServiceInstance = new CalculatorService(logger);
-    const result = calcularServiceInstance.add(2, 2);
+    console.log('calling first each');
+
+    const result = calculator.add(2, 2);
     expect(result).toBe(4);
     // número de vezes que o serviço foi chamado
-    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should substract two numbers', () => {
+    console.log('calling second each');
+
     const calcularServiceInstance = new CalculatorService(new LoggerService());
     const result = calcularServiceInstance.subtract(2, 2);
     expect(result).toBe(0);
