69190ce0bd95e56825918e8ebfec788ee13e93c0
MJWORKS <mjworks2020@outlook.com>
2022-07-16 13:58:28 -0300

    Added new example using jasmine.createSpyObj

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,11 +2,13 @@ import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 describe(CalculatorService.name, () => {
   it('should add two numbers', () => {
-    const logger = new LoggerService();
-    spyOn(logger, 'log');
+    const logger = jasmine.createSpyObj('LoggerService', ['log']);
+    // vc pode utilizar para enviar como retorno um mock de dados
+    //logger.log.and.returnValue();
     const calcularServiceInstance = new CalculatorService(logger);
     const result = calcularServiceInstance.add(2, 2);
     expect(result).toBe(4);
+    // número de vezes que o serviço foi chamado
     expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
