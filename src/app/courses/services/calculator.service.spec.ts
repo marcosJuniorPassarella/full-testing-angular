64084b0b7649b81cb1db562bdea1b4694658b603
MJWORKS <mjworks2020@outlook.com>
2022-07-16 13:14:56 -0300

    Simple test example

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,10 +1,15 @@
 import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
 describe(CalculatorService.name, () => {
   it('should add two numbers', () => {
-    pending();
+    const calcularServiceInstance = new CalculatorService(new LoggerService());
+    const result = calcularServiceInstance.add(2, 2);
+    expect(result).toBe(4);
   });
 
   it('should substract two numbers', () => {
-    pending();
+    const calcularServiceInstance = new CalculatorService(new LoggerService());
+    const result = calcularServiceInstance.subtract(2, 2);
+    expect(result).toBe(0);
   });
 });
