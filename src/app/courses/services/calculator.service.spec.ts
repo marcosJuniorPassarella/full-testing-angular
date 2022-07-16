588351a7909fbaf46c568999f9e9f76a25c4f3f4
MJWORKS <mjworks2020@outlook.com>
2022-07-16 13:50:54 -0300

    Added new simple test example using spyOn

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,9 +2,12 @@ import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 describe(CalculatorService.name, () => {
   it('should add two numbers', () => {
-    const calcularServiceInstance = new CalculatorService(new LoggerService());
+    const logger = new LoggerService();
+    spyOn(logger, 'log');
+    const calcularServiceInstance = new CalculatorService(logger);
     const result = calcularServiceInstance.add(2, 2);
     expect(result).toBe(4);
+    expect(logger.log).toHaveBeenCalledTimes(1);
   });
 
   it('should substract two numbers', () => {
