e81423adf3d37303cdb5758c28dcfb31a39a6cff
MJWORKS <mjworks2020@outlook.com>
2022-07-19 10:16:27 -0300

    Added TestBed.configureTestingModule configuration

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,29 +1,31 @@
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
 describe(CalculatorService.name, () => {
   let calculator: CalculatorService;
   let loggerSpy: any;
 
   beforeEach(() => {
-    console.log('calling before each');
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+    calculator = TestBed.inject(CalculatorService);
   });
 
   it('should add two numbers', () => {
-    console.log('calling first each');
-
     const result = calculator.add(2, 2);
     expect(result).toBe(4);
-    // número de vezes que o serviço foi chamado
     expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should substract two numbers', () => {
-    console.log('calling second each');
-
     const calcularServiceInstance = new CalculatorService(new LoggerService());
     const result = calcularServiceInstance.subtract(2, 2);
     expect(result).toBe(0);
   });
 });
+ 
