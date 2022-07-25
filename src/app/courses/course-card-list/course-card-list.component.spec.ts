356ea41becbab06ed81571500b5a831cfe82d7c1
MJWORKS <mjworks2020@outlook.com>
2022-07-25 20:35:24 -0300

    Added should display the course list test

--- src/app/courses/course-card-list/course-card-list.component.spec.ts
+++ src/app/courses/course-card-list/course-card-list.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CourseCardListComponent } from './course-card-list.component';
 import { CoursesModule } from '../courses.module';
+import { setupCourses } from '../common/setup-test-data';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('CourseCardListComponent', () => {
   let component: CourseCardListComponent;
   let fixture: ComponentFixture<CourseCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
@@ -15,6 +19,7 @@ describe('CourseCardListComponent', () => {
       .then(() => {
         fixture = TestBed.createComponent(CourseCardListComponent);
         component = fixture.componentInstance;
+        el = fixture.debugElement;
       });
   });
 
@@ -23,7 +28,11 @@ describe('CourseCardListComponent', () => {
   });
 
   it('should display the course list', () => {
-    pending();
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const cards = el.queryAll(By.css('.course-card'));
+    expect(cards).toBeTruthy('Could not find cards');
+    expect(cards.length).toBe(12, 'Unexpected number of courses');
   });
 
   it('should display the first course', () => {
--- src/app/courses/home/sort-course-by-seq.ts
+++ src/app/courses/home/sort-course-by-seq.ts
@@ -1,7 +1,4 @@
-import {Course} from '../model/course';
-
-
-export function sortCoursesBySeqNo(c1:Course, c2: Course) {
+export function sortCoursesBySeqNo(c1:any, c2: any) {
   return c1.seqNo - c2.seqNo;
 
 }
