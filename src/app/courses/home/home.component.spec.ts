4150a1771aa1e5d630960fe7934f512e03194fe2
MJWORKS <mjworks2020@outlook.com>
2022-07-26 14:08:23 -0300

    Added should display only beginner courses test

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,19 +1,21 @@
-import {
-  async,
-  ComponentFixture,
-  TestBed,
-  waitForAsync,
-} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { DebugElement } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { CoursesModule } from '../courses.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { CoursesService } from '../services/courses.service';
+import { setupCourses } from '../common/setup-test-data';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
   let component: HomeComponent;
   let el: DebugElement;
+  let coursesService: any;
+  const beginnerCourses = setupCourses().filter(
+    (course) => course.category == 'BEGINNER'
+  );
 
   beforeEach(waitForAsync(() => {
     const coursesServiceSpy = jasmine.createSpyObj('CoursesService', [
@@ -34,6 +36,7 @@ describe('HomeComponent', () => {
         fixture = TestBed.createComponent(HomeComponent);
         component = fixture.componentInstance;
         el = fixture.debugElement;
+        coursesService = TestBed.inject(CoursesService);
       });
   }));
 
@@ -42,7 +45,10 @@ describe('HomeComponent', () => {
   });
 
   it('should display only beginner courses', () => {
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
+    fixture.detectChanges();
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+    expect(tabs.length).toBe(1);
   });
 
   it('should display only advanced courses', () => {
