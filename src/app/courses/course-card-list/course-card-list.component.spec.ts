c33c38f354dc003dd90b1f351a1bc18aec9be26a
MJWORKS <mjworks2020@outlook.com>
2022-07-26 10:15:09 -0300

    Added new component test

--- src/app/courses/course-card-list/course-card-list.component.spec.ts
+++ src/app/courses/course-card-list/course-card-list.component.spec.ts
@@ -36,6 +36,15 @@ describe('CourseCardListComponent', () => {
   });
 
   it('should display the first course', () => {
-    pending();
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const course = component.courses[0];
+    const card = el.query(By.css('.course-card:first-child')),
+      title = card.query(By.css('mat-card-title')),
+      image = card.query(By.css('img'));
+
+    expect(card).toBeTruthy('Could not find course card');
+    expect(title.nativeElement.textContent).toBe(course.titles.description);
+    expect(image.nativeElement.src).toBe(course.iconUrl);
   });
 });
