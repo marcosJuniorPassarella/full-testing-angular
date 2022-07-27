b1c072248c8313496391a556295327182c2a2b96
MJWORKS <mjworks2020@outlook.com>
2022-07-27 09:56:37 -0300

    Added new test using fake async and tick

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,5 +1,6 @@
-fdescribe('Async Testing Examples', () => {
+import { fakeAsync, tick } from '@angular/core/testing';
 
+fdescribe('Async Testing Examples', () => {
   it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
     let test = false;
     setTimeout(() => {
@@ -9,4 +10,14 @@ fdescribe('Async Testing Examples', () => {
       done();
     }, 1000);
   });
+
+  it('Asynchronous test example - setTimeout()', fakeAsync(() => {
+    let test = false;
+    setTimeout(() => {
+      console.log('runing assertions');
+      test = true;
+      expect(test).toBeTruthy();
+    }, 1000);
+    tick(1000);
+  }));
 });
