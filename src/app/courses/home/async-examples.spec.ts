3720ea368fe0d233f6c698a6c2b49f86b330e46b
MJWORKS <mjworks2020@outlook.com>
2022-07-27 09:17:18 -0300

    Created initial async test

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,8 +1,10 @@
-
-describe("Async Testing Examples", () =>{
-
-    it('Asynchronous test example with Jasmine done()', () => {
-        
-    });
-    
-})
+describe('Async Testing Examples', () => {
+  it('Asynchronous test example with Jasmine done()', () => {
+    let test = false;
+    setTimeout(() => {
+      console.log('runing assertions');
+      test = true;
+      expect(test).toBeTruthy();
+    }, 1000);
+  });
+});
