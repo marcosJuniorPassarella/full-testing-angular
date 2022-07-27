304b36e2c22e29be4e04b32ecda80d5b3f200e7b
MJWORKS <mjworks2020@outlook.com>
2022-07-27 09:22:41 -0300

    Use jasmine done in the test

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,10 +1,12 @@
-describe('Async Testing Examples', () => {
-  it('Asynchronous test example with Jasmine done()', () => {
+fdescribe('Async Testing Examples', () => {
+
+  it('Asynchronous test example with Jasmine done()', (done: DoneFn) => {
     let test = false;
     setTimeout(() => {
       console.log('runing assertions');
       test = true;
       expect(test).toBeTruthy();
+      done();
     }, 1000);
   });
 });
